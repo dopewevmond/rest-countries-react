9a479084e1de519da0ac313413678721333b7785
dopewevmond <dopewevmond@gmail.com>
2022-11-27 11:39:12 +0000

    added react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css'
 import ThemeContext from './helpers/Theme'
 import Navbar from './components/Navbar/Navbar'
 import CountriesPage from './components/CountriesPage/CountriesPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CountryDetail from './components/CountryDetail/CountryDetail'
 
 const App = () => {
   const [T, toggleT] = useState('light')
@@ -15,10 +17,15 @@ const App = () => {
     }
   }
   return (
-    <ThemeContext.Provider value={{theme: T, toggleTheme: togglerFunc}}>
-      <Navbar />
-      <CountriesPage />
-    </ThemeContext.Provider>
+    <BrowserRouter>
+      <ThemeContext.Provider value={{theme: T, toggleTheme: togglerFunc}}>
+        <Navbar />
+      <Routes>
+        <Route path='/' element={ <CountriesPage /> } />
+        <Route path='/country' element={ <CountryDetail /> } />
+      </Routes>
+      </ThemeContext.Provider>
+    </BrowserRouter>
   )
 }
 
