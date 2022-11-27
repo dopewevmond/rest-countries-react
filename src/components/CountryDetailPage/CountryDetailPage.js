509c28639400443314c46d8ebc3a0d87f256d783
dopewevmond <dopewevmond@gmail.com>
2022-11-27 13:34:17 +0000

    softcoded country detail component

--- src/components/CountryDetailPage/CountryDetailPage.js
+++ src/components/CountryDetailPage/CountryDetailPage.js
@@ -1,73 +1,113 @@
 import './CountryDetailPage.css'
 import ThemeContext from '../../helpers/Theme'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import NeighborCountry from '../NeighborCountry/NeighborCountry'
 
-const CountryDetailPage = () => (
-  <ThemeContext.Consumer>
-    {/* eslint-disable-next-line no-unused-vars */}
-    {({ theme, toggleTheme }) => (
-      <div className={theme == 'light' ?
-                    'countries-bg-light full-height' : 
-                    'countries-bg-dark full-height'}
-      >
-        <div className='container pt-1'>
-          <div className='mb-3'>
-            <span className={theme == 'light' ? 
-                        'navbar-bg-light' : 
-                        'navbar-bg-dark'}
-            >
-              <Link to='/' className='country-detail-back-btn'>Back</Link>
-            </span>
-          </div>
-          
-          <div className='flex-2-cols'>
-            <div className='country-detail-img-container flex-child-small'>
-              <img src='https://flagcdn.com/w320/mr.png' alt='country flag' className=''/>
+const CountryDetailPage = () => {
+  const { countryName } = useParams()
+  const [countryDetails, setCountryDetails] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCountryDetails = async () => {
+      const countryDetailsResponse = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      const countryDetails = await countryDetailsResponse.json()
+      return countryDetails[0]
+    }
+    fetchCountryDetails()
+      .then((cd) => {
+        setCountryDetails({
+          name: cd.name.common,
+          nativeName: cd.name.official,
+          population: cd.population,
+          region: cd.region,
+          subregion: cd.subregion || 'N/A',
+          capital: (cd.capital && cd.capital[0]) || 'N/A',
+          tld: cd.tld[0],
+          currencies: cd.currencies || 'N/A',
+          languages: cd.languages.eng || 'N/A',
+          borderCountries: cd.borders,
+          flag: cd.flags.png
+        })
+        console.log(countryDetails)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <ThemeContext.Consumer>
+      {/* eslint-disable-next-line no-unused-vars */}
+      {({ theme, toggleTheme }) => (
+        <div className={theme == 'light' ?
+                      'countries-bg-light full-height' : 
+                      'countries-bg-dark full-height'}
+        >
+          {loading === false ?
+          (
+            <div className='container pt-1'>
+              <div className='mb-3'>
+                <span className={theme == 'light' ? 
+                            'navbar-bg-light' : 
+                            'navbar-bg-dark'}
+                >
+                  <Link to='/' className='country-detail-back-btn'>Back</Link>
+                </span>
+              </div>
+              
+              <div className='flex-2-cols'>
+                <div className='country-detail-img-container flex-child-small'>
+                  <img src={countryDetails.flag} alt='country flag' className=''/>
+                </div>
+
+                <div className='country-detail-details-container flex-child-large'>
+                  <div className='country-detail-big-text mb-1 sm-pt-1'><strong>{countryDetails.name}</strong></div>
+                  <div className='flex-2-cols align-items-start country-detail-small-text mb-1'>
+                    <div className='sm-mb-1'>
+                      <div className='mb-half'><strong>Native name: </strong>{countryDetails.nativeName}</div>
+                      <div className='mb-half'><strong>Population: </strong>{countryDetails.population}</div>
+                      <div className='mb-half'><strong>Region: </strong>{countryDetails.region}</div>
+                      <div className='mb-half'><strong>Sub-region: </strong>{countryDetails.subregion}</div>
+                      <div className='mb-half'><strong>Capital: </strong>{countryDetails.capital}</div>
+                    </div>
+                    <div className='sm-mb-1'>
+                      <div className='mb-half'><strong>Top Level Domain: </strong>{countryDetails.tld}</div>
+                      <div className='mb-half'><strong>Currencies: </strong>{Object.keys(countryDetails.currencies).map((currency, idx) => (<span key={idx}>{countryDetails.currencies[currency].name}</span>))}</div>
+                      <div className='mb-half'><strong>Languages: </strong>{countryDetails.languages}</div>
+                    </div>
+                  </div>
+                  {/* end of flex column */}
+
+                  <div className='country-detail-small-text sm-pb-4'>
+                  <div className='mb-half country-detail-flex-colmobile-rowtab'>
+                    <span className='mr-1'><strong>Border countries:</strong></span>
+                    <div className='border-countries-container sm-mt-1'>
+                      {
+                        [countryDetails.borderCountries].map((border, idx) => (
+                          <NeighborCountry name={border} key={idx} />
+                        ))
+                      }
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
             </div>
-
-            <div className='country-detail-details-container flex-child-large'>
-              <div className='country-detail-big-text mb-1 sm-pt-1'><strong>Mauritania</strong></div>
-              <div className='flex-2-cols align-items-start country-detail-small-text mb-1'>
-                <div className='sm-mb-1'>
-                  <div className='mb-half'><strong>Native name: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Population: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Region: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Sub-region: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Capital: </strong>Mauritania</div>
-                </div>
-                <div className='sm-mb-1'>
-                  <div className='mb-half'><strong>Top Level Domain: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Currencies: </strong>Mauritania</div>
-                  <div className='mb-half'><strong>Languages: </strong>Mauritania</div>
-                </div>
-              </div>
-              {/* end of flex column */}
-
-              <div className='country-detail-small-text sm-pb-4'>
-              <div className='mb-half country-detail-flex-colmobile-rowtab'>
-                <span className='mr-1'><strong>Border countries:</strong></span>
-                <div className='border-countries-container sm-mt-1'>
-                  {
-                    ['Ghana', 'Togo', 'Ivory Coast'].map((country, idx) => (
-                      <span className=
-                        {theme == 'light' ?
-                          'navbar-bg-light country-detail-bordercountry-box mr-half' :
-                          'navbar-bg-dark country-detail-bordercountry-box mr-half'}
-                        key={idx}
-                      >
-                        {country}
-                      </span>
-                    ))
-                  }
-                </div>
-              </div>
-              </div>
-            </div>
-          </div>
+          ) : 
+          (
+            <div>loading</div>
+          )
+          }
         </div>
-      </div>
-    )}
-  </ThemeContext.Consumer>
-)
+      )}
+    </ThemeContext.Consumer>
+  )
+}
 
 export default CountryDetailPage
