494ee68632a3b10d3ee24261bf0e543a34d27085
dopewevmond <dopewevmond@gmail.com>
2022-11-28 00:24:31 +0000

    (search and filter) component

---
+++ src/components/SearchFilter/SearchFilter.js
@@ -0,0 +1,38 @@
+import './SearchFilter.css'
+import ThemeContext from "../../helpers/Theme"
+
+const SearchFilter = () => (
+  <ThemeContext.Consumer>
+    {/* eslint-disable-next-line no-unused-vars */}
+    {({ theme, toggleTheme }) => (
+      <div className='searchfilter-flex-container pt-1 pb-1'>
+        <div className={theme == 'light' ?
+                      'navbar-bg-light searchfilter-searchbox-container' :
+                      'navbar-bg-dark searchfilter-searchbox-container'}
+        >
+          <input
+            className='input-no-styles'
+            type="text"
+            placeholder='Search for a country...'
+          />
+        </div>
+
+        <span className={theme == 'light' ?
+                      'navbar-bg-light searchfilter-filterbox-container' :
+                      'navbar-bg-dark searchfilter-filterbox-container'}
+        >
+          <select name='region' className='input-no-styles'>
+            <option value=''>All Regions</option>
+            <option value='africa'>Africa</option>
+            <option value='americas'>Americas</option>
+            <option value='asia'>Asia</option>
+            <option value='europe'>Europe</option>
+            <option value='oceania'>Oceania</option>
+          </select>
+        </span>
+      </div>
+    )}
+  </ThemeContext.Consumer>
+)
+
+export default SearchFilter
